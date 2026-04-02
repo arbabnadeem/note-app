8ca0cc6c059c8a931ac2091591eef4b48f355015
Arbab Nadeem <arbabnadim50@gmail.com>
2026-04-02 21:32:12 +0530

    second-commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const noteModel = require("./models/noteModel");
 const connectDB = async () => {
   try {
     mongoose.connection.on("connected", () => console.log("db connected"));
-    await mongoose.connect(config.connectionString);
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.log(error.message);
   }
